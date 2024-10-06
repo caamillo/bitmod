61e659e0f72bfe812a7cb53984b65a3547df7d56
caamillo <mcamillolud@gmail.com>
2024-10-06 20:30:03 +0200

    try catch

--- cron.js
+++ cron.js
@@ -5,17 +5,21 @@ const { bookDevice } = require('./api/booking')
 const BOOKING_EXPIRY = 10 * 60; // 10 minutes in seconds
 
 const checkBookings = async () => {
-    const keys = await redis.keys('booking:*'); // Fetch all booking keys
-    for (const key of keys) {
-        let status = JSON.parse(await redis.get(key));
-        if (!status?.booking) continue
-        const d = new Date(status.pushed_at)
-        if (new Date() < d + BOOKING_EXPIRY) continue
-
-        const device = status.device
-        const token = key.split(':')[1]
-        console.log(`Re-booking device ${device} of user ${token}`)
-        await bookDevice(token, device)
+    try {
+        const keys = await redis.keys('booking:*'); // Fetch all booking keys
+        for (const key of keys) {
+            let status = JSON.parse(await redis.get(key));
+            if (!status?.booking) continue
+            const d = new Date(status.pushed_at)
+            if (new Date() < d + BOOKING_EXPIRY) continue
+    
+            const device = status.device
+            const token = key.split(':')[1]
+            console.log(`Re-booking device ${device} of user ${token}`)
+            await bookDevice(token, device)
+        }
+    } catch (err) {
+        console.log(err)
     }
 }
 
